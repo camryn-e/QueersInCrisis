5a143f09374e127fc55d18e4573b4ac877751cbc
ahaub <46764227+ahaub@users.noreply.github.com>
2020-11-12 13:26:04 -0800

    Add files via upload

---
+++ crisis.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import csv
+from gsheets import Sheets
+
+sheets = Sheets.from_files('client_secret.json', '~/storage.json')
+url = "https://docs.google.com/spreadsheets/d/1GgDcl-iD8lXbsuHBqD7hvjYW5PJY8xNAImIwfLy212w/edit#gid=1686916710"
+s = sheets.get(url)
+
+class Organization:
+    
+    def __init__(self, name, contact, email, phone, address, website, state,
+                 crisis, counseling, peer, health, addiction, homelessness,
+                 advocacy, medication, other, notes, etc):
+        self.name = name
+        self.contact = contact
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.website = website
+        self.state = state
+        self.crisis = crisis
+        self.counseling = counseling
+        self.peer = peer
+        self.health = health
+        self.addiction = addiction
+        self.homelessness = homelessness
+        self.advocacy = advocacy
+        self.medication = medication
+        self.other = other
+        self.notes = notes
+        self.etc = etc
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'contact': self.contact,
+            'email': self.email,
+            'phone': self.phone,
+            'address': self.address,
+            'website': self.website,
+            'state': self.state,
+            'crisis': self.crisis,
+            'counseling': self.counseling,
+            'peer': self.peer,
+            'health': self.health,
+            'addiction': self.addiction,
+            'homelessness': self.homelessness,
+            'advocacy': self.advocacy,
+            'medication': self.medication,
+            'other': self.other,
+            'notes': self.notes,
+            'etc': self.etc,
+            }
+
+
+if __name__ == "__main__":
+    orglist=[]
+    with open('crisis.csv') as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        for a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r in reader:
+            orglist.append(Organization(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r))
+
+    df = pd.DataFrame.from_records([s.to_dict() for s in orglist])
