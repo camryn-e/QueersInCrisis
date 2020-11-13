56f22a5ba96ae27f2f4acfcb8983b904fe8c1669
Emmett Leddin <52934003+emleddin@users.noreply.github.com>
2020-11-12 22:30:11 -0600

    Add datatables render

--- crisis.py
+++ crisis.py
@@ -1,8 +1,43 @@
 import pandas as pd
 import csv
 
+## From https://stackoverflow.com/questions/50807744/apply-css-class-to-pandas-dataframe-using-to-html
+## Set up minimum HTML required for DataTable in iFrame
+## You need to double the braces for proper Python rendering of javascript
+## function
+html_string = '''
+<html lang="en-us">
+ <link rel="shortcut icon" href='https://clubrunner.blob.core.windows.net/00000305556/Favicon/favicon.ico?time=637407227082223373' />
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <link href="http://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
+  <script src="http://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){{
+            $('.dataframe').DataTable( {{
+                paging: true,
+                stateSave: true,
+                searching: true
+            }});
+         }});
+    </script>
+<style>
+title.hidden {{
+  visibility: hidden;
+}}
+html {{
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+}}
+</style>
+<!-- Hidden Title -->
+<title class="hidden">Crisis Resource Table</title>
+  <body>
+    {table}
+  </body>
+</html>
+'''
+
 class Organization:
-    
+
     def __init__(self, name, contact, email, phone, address, website, state,
                  crisis, counseling, peer, health, addiction, homelessness,
                  advocacy, medication, other, notes, etc):
@@ -47,7 +82,6 @@ class Organization:
             'etc': self.etc,
             }
 
-
 if __name__ == "__main__":
     orglist=[]
     with open('crisis.csv') as f:
@@ -57,4 +91,11 @@ if __name__ == "__main__":
             orglist.append(Organization(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r))
 
     df = pd.DataFrame.from_records([s.to_dict() for s in orglist])
-    df.to_html()
+
+    ## Save df as an HTML table with customized style
+    ## "display" is a data.tables default with striped rows
+    ## you don't need table_id, but might be helpful for other iFrame approaches
+    with open('docs/crisis-resources-datatable.html', 'w') as f:
+        f.write(html_string.format(table=df.to_html(index=False, \
+         classes='display', escape=False, table_id="CRtable")))
+        f.close()
